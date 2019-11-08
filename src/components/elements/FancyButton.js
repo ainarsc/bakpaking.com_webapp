29a4c263e2ed28cf4b7d72fae919bc29133eeb11
Ainars Ciesa <ainars.c@gmail.com>
2019-11-08 00:56:08 +0000

    Add lading page and the customzied button

---
+++ src/components/elements/FancyButton.js
@@ -0,0 +1,75 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
+
+const Button = styled.button`
+  padding: 20px 30px;
+  font-size: 30px;
+  cursor: pointer;
+  position: relative;
+  border: 2px solid #f2f2f2;
+  outline: none;
+  color: #f2f2f2;
+  background-color: transparent;
+  margin: 1.5rem;
+  white-space: nowrap;
+
+  &::after,
+  &::before,
+  span::after,
+  span::before {
+    content: "";
+    position: absolute;
+    background-color: #f2f2f2;
+    transition: 0.5s ease;
+  }
+  &::after,
+  &::before {
+    height: 100%;
+    width: 2px;
+    top: -2px;
+  }
+  &::after {
+    left: -2px;
+  }
+  &::before {
+    right: -2px;
+  }
+  span::after,
+  span::before {
+    height: 2px;
+    width: 100%;
+    left: -2px;
+  }
+  span::after {
+    top: -2px;
+  }
+  span::before {
+    bottom: -2px;
+  }
+  &:hover:after {
+    transform: translatex(-12px);
+  }
+  &:hover:before {
+    transform: translatex(12px);
+  }
+  &:hover span::after {
+    transform: translatey(-12px);
+  }
+  &:hover span::before {
+    transform: translatey(12px);
+  }
+`
+
+const FancyButton = ({ children, path }) => {
+  return (
+    <Link to={path}>
+      <Button>
+        {children}
+        <span>{/* Required for button to work properly */}</span>
+      </Button>
+    </Link>
+  )
+}
+
+export default FancyButton
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,85 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
+import React, { Fragment } from "react"
+import styled from "styled-components"
+// import { Link } from "gatsby"
 import SEO from "../components/seo"
+import FancyButton from "../components/elements/FancyButton"
+import landingImage from "../images/me.jpg"
+import GlobalStyles from "../global"
+// import Layout from "../components/layout"
+// import Image from "../components/image"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const BackgroundImage = styled.section`
+  height: 100vh;
+  position: relative;
+  background: linear-gradient(
+      to right,
+      rgba(255, 255, 255, 0.3),
+      rgba(255, 255, 255, 0.3)
+    ),
+    url(${props => props.img});
+  background-size: cover;
+  background-position: center;
+`
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`
+
+const Title = styled.h1`
+  font-size: 2.5rem;
+  color: #f2f2f2;
+  text-align: center;
+  font-weight: lighter;
+  padding: 1.5rem;
+`
+
+const Highlight = styled.div`
+  background: rgba(0, 0, 0, 0.5);
+  border-radius: 5px;
+  justify-content: center;
+`
+
+const Line = styled.div`
+  height: 1px;
+  background: #f2f2f2;
+  width: 60%;
+  margin: auto;
+`
+
+const IndexPage = () => {
+  return (
+    <Fragment>
+      <GlobalStyles />
+      <SEO title="Home" />
+      <BackgroundImage img={landingImage}>
+        <Container>
+          <Highlight>
+            <Title>No BS guide to travel</Title>
+            <Line />
+            <FancyButton path="/blog">Enter</FancyButton>
+          </Highlight>
+        </Container>
+      </BackgroundImage>
+    </Fragment>
+  )
+}
+
+// <Layout>
+//
+//   <h1>Hi people</h1>
+//   <p>Welcome to your new Gatsby site.</p>
+//   <p>Now go build something great.</p>
+//   <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+//     <Image />
+//   </div>
+//   <Link to="/page-2/">Go to page 2</Link>
+// </Layout>
 
 export default IndexPage
