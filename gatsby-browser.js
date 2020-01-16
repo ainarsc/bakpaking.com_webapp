b5b1f28e0e96380c65e36b31dee79ad6dac17cb2
Ainars Ciesa <ainars.c@gmail.com>
2020-01-16 02:12:04 +0000

    Back button bug fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,15 @@
 import wrapWithProvider from "./wrap-with-provider"
+import { globalHistory } from "@reach/router"
+
 import "typeface-raleway"
 import "typeface-amatic-sc"
 export const wrapRootElement = wrapWithProvider
 export const onServiceWorkerUpdateReady = () => window.location.reload(true)
+export const onInitialClientRender = () => {
+  /**
+   * This is a workaround for a bug in Gatsby
+   *
+   * See https://github.com/gatsbyjs/gatsby/issues/8357 for more details
+   */
+  globalHistory._onTransitionComplete()
+}
